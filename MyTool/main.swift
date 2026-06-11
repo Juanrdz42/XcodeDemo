1eabe87aa7cb0307a1c7fccf4484694333ef3b92
Iván Gabriel Espinosa García <a01178743@tec.mx>
2026-06-11 16:33:47 -0600

    Add French German Italian and Spanish output

--- MyTool/main.swift
+++ MyTool/main.swift
@@ -1,5 +1,13 @@
 import Foundation
 
-print("Hello, World!")
+print("French: Bonjour, le monde !")
+print("French: Bienvenue dans ma vie !")
 
-print("Welcome to my life!")
+print("German: Hallo, Welt!")
+print("German: Willkommen in meinem Leben!")
+
+print("Italian: Ciao, mondo!")
+print("Italian: Benvenuto nella mia vita!")
+
+print("Spanish: Hola, mundo!")
+print("Spanish: Bienvenido a mi vida!")
